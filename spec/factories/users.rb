91076c6eb759a49a89701dc58954dd4677e80e65
yuhorn <makkachin3@gmail.com>
2023-03-08 16:06:30 +0900

    Userモデル単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    transient do
+      person {Gimei.name}
+    end
+    last_name             {person.last.kanji}
+    first_name            {person.first.kanji}
+    last_name_kana        {person.last.katakana}
+    first_name_kana       {person.first.katakana}
+    birth_date            {Faker::Date.backward}
+  end
+end
