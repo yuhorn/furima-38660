9bc0cbb89613a237e8180641f2e49b4da6df40c7
yuhorn <makkachin3@gmail.com>
2023-03-14 21:43:32 +0900

    レビュー後任意実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,33 +16,43 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include("Item name can't be blank")
         end
+        it "item_nameは40文字以内でないと出品できない" do
+          @item.item_name = Faker::Lorem.characters(number: 41)
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Item name is too long (maximum is 40 characters)")
+        end
         it "item_infoが存在しないと出品できない" do
           @item.item_info = ''
           @item.valid?
           expect(@item.errors.full_messages).to include("Item info can't be blank")
         end
+        it "item_infoは1000文字以内でないと出品できない" do
+          @item.item_info = Faker::Lorem.characters(number: 1001)
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Item info is too long (maximum is 1000 characters)")
+        end
         it "item_category_idが未選択のままだと出品できない" do
-          @item.item_category_id = '1'
+          @item.item_category_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Item category can't be blank")
         end
         it "item_sales_status_idが未選択のままだと出品できない" do
-          @item.item_sales_status_id = '1'
+          @item.item_sales_status_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Item sales status can't be blank")
         end
         it "item_shipping_fee_idが未選択のままだと出品できない" do
-          @item.item_shipping_fee_status_id = '1'
+          @item.item_shipping_fee_status_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Item shipping fee status can't be blank")
         end
         it "prefecture_idが未選択のままだと出品できない" do
-          @item.prefecture_id = '1'
+          @item.prefecture_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Prefecture can't be blank")
         end
         it "item_schedule_delivery_idが未選択のままだと出品できない" do
-          @item.item_schedule_delivery_id = '1'
+          @item.item_schedule_delivery_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Item schedule delivery can't be blank")
         end
@@ -52,12 +62,12 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include("Item price can't be blank")
         end
         it "item_priceは¥300以上でないと出品できない" do
-          @item.item_price = '299'
+          @item.item_price = 299
           @item.valid?
           expect(@item.errors.full_messages).to include("Item price must be greater than or equal to 300")
         end
         it "item_priceは¥9,999,999以下でないと出品できない" do
-          @item.item_price = '10000000'
+          @item.item_price = 10000000
           @item.valid?
           expect(@item.errors.full_messages).to include("Item price must be less than or equal to 9999999")
         end
@@ -65,16 +75,6 @@ RSpec.describe Item, type: :model do
           @item.item_price = '３００'
           @item.valid?
           expect(@item.errors.full_messages).to include("Item price is not a number")
-        end
-        it "item_nameは40文字以内でないと出品できない" do
-          @item.item_name = Faker::Lorem.characters(number: 41)
-          @item.valid?
-          expect(@item.errors.full_messages).to include("Item name is too long (maximum is 40 characters)")
-        end
-        it "item_infoは1000文字以内でないと出品できない" do
-          @item.item_info = Faker::Lorem.characters(number: 1001)
-          @item.valid?
-          expect(@item.errors.full_messages).to include("Item info is too long (maximum is 1000 characters)")
         end
         it "imageが紐付いていない場合出品できない" do
           @item.image = nil
