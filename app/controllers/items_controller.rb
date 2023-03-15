aecfcd3bbfa88cfbf536f66050a47b085ab9691e
yuhorn <makkachin3@gmail.com>
2023-03-15 20:18:18 +0900

    商品詳細表示機能（売却済み条件分岐以外）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
+    @items = Item.all
   end
 
   def new
@@ -14,6 +15,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
