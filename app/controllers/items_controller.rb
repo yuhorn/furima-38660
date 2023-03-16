1d49b5ab50d7a1d6a5d9a2bfde7633355f740724
yuhorn <makkachin3@gmail.com>
2023-03-16 20:29:07 +0900

    商品情報編集機能（売却済み条件分岐以外）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :item_info, :item_category_id, :item_sales_status_id, :prefecture_id, :item_schedule_delivery_id, :item_shipping_fee_status_id, :item_price).merge(user_id: current_user.id)
