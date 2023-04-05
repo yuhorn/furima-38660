94d0667dce8c40bcb050083ac2f206ba35518010
yuhorn <makkachin3@gmail.com>
2023-04-05 19:43:52 +0900

    一つ前の変更を取消

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,10 +8,10 @@ worker_processes 1
 working_directory app_path
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/var/run/pids/unicorn.pid"
+pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen "#{app_path}/var/run/sockets/unicorn.sock"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
