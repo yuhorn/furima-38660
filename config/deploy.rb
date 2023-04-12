1afa5b3a29ddc8fd7a4c283cf40dae568bbc34ea
yuhorn <makkachin3@gmail.com>
2023-04-13 01:30:10 +0900

    mainブランチを指定

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,9 @@ set :application, 'memoria'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yuhorn/memoria.git'
 
+# ブランチを指定する
+set :branch, "main"
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
