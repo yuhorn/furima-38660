daae79fc291c41c6b722346ccfe1205dab946d34
yuhorn <makkachin3@gmail.com>
2023-04-21 21:58:59 +0900

    選択した画像情報を取得

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,5 +2,11 @@ document.addEventListener('DOMContentLoaded', function(){
   const postForm = document.getElementById('item-image')
   const previewList = document.getElementById('previews')
   if (!postForm) return null
-  console.log("preview.jsが読み込まれました")
-});
+
+  const fileField = document.querySelector('input[type="file"][name="item[image]"]')
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', function(e){
+    const file = e.target.files[0]
+    const blob = window.URL.createObjectURL(file)
+  })
+})
