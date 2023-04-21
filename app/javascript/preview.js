986f0234097e7304eb82d723c6b8ec66459f9417
yuhorn <makkachin3@gmail.com>
2023-04-21 22:09:33 +0900

    プレビュー表示実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -8,5 +8,12 @@ document.addEventListener('DOMContentLoaded', function(){
   fileField.addEventListener('change', function(e){
     const file = e.target.files[0]
     const blob = window.URL.createObjectURL(file)
+    const previewWrapper = document.createElement('div')
+    previewWrapper.setAttribute('class', 'preview')
+    const previewImage = document.createElement('img')
+    previewImage.setAttribute('class', 'preview-image')
+    previewImage.setAttribute('src', blob)
+    previewWrapper.appendChild(previewImage)
+    previewList.appendChild(previewWrapper)
   })
 })
