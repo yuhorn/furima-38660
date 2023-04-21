229a46de5e2b20c25cba028caedb2775f71ea599
yuhorn <makkachin3@gmail.com>
2023-04-21 21:42:48 +0900

    実装準備

---
+++ app/javascript/preview.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const postForm = document.getElementById('item-image')
+  const previewList = document.getElementById('previews')
+  if (!postForm) return null
+  console.log("preview.jsが読み込まれました")
+});
